154539913ebb004b6ad5b5660325d6d6a6b838bf
Knob <59198478+nbr41to@users.noreply.github.com>
2022-04-30 23:13:35 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  plugins: ['unused-imports'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'no-console': ['error'],
+    'import/order': ['error'],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.stories.tsx', 'src/pages/**/*'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['_app.tsx'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
+      },
+    },
+    {
+      files: ['*.stories.tsx', '*.tsx'],
+      rules: {
+        'react/jsx-filename-extension': 'off',
+      },
+    },
+  ],
+};
