9485a4a5f057d9595ac6d01170ce93880760b011
Knob <59198478+nbr41to@users.noreply.github.com>
2022-05-04 20:01:52 +0900

    Update LoadingScreen.tsx

--- src/components/model/Screen/LoadingScreen.tsx
+++ src/components/model/Screen/LoadingScreen.tsx
@@ -6,7 +6,7 @@ export const LoadingScreen: FC = () => {
   return (
     <div className="flex justify-center gap-4 outline-none">
       <div className="relative h-[600px] w-[600px] bg-white outline-none ring-2 ring-slate-600" />
-      <FieldInfo message="" />
+      <FieldInfo message="" coordinate={{ x: 0, y: 0 }} />
     </div>
   );
 };
