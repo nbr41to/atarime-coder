a6f0bef6596114d439baa6fa8ae08157bd5a9dd2
Knob <59198478+nbr41to@users.noreply.github.com>
2022-05-06 16:02:22 +0900

    OGPの追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,11 @@ import Head from 'next/head';
 import { Layout } from 'src/components/ui/Layout';
 import 'styles/globals.css';
 
+const title = 'あたりめコーダー';
+const description = 'イカに転生してプログラミングを学習するゲーム';
+const url = 'https://atarime-coder.vercel.app/';
+const image = '/ogp.png';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
@@ -13,7 +18,22 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <title>あたりめコーダー</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="image" content={image} />
+        <meta property="og:type" content="website" key="og:type" />
+        <meta property="og:url" content={url} key="og:url" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} key="og:image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={url + image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content={title} />
+
+        <title>{title}</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
